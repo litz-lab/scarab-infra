202298680a02d62b605c7a6fae7f79c3786bf2b9
Alexander Symons <aesymons888@gmail.com>
2025-03-01 23:36:00 +0000

    Tried adding commands to entrypoint

--- workloads/spec2017/workload_root_entrypoint.sh
+++ workloads/spec2017/workload_root_entrypoint.sh
@@ -4,3 +4,6 @@ APPNAME="$1"
 
 mkdir -p $tmpdir/cpu2017_install
 sudo mount -t iso9660 -o ro,exec,loop $tmpdir/cpu2017-1_0_5.iso $tmpdir/cpu2017_install
+cd $tmpdir/cpu2017_install && echo "yes" | ./install.sh -d $tmpdir/cpu2017
+cp $tmpdir/memtrace.cfg $tmpdir/cpu2017/config/memtrace.cfg
+cp $tmpdir/compile-538-clang.sh $tmpdir/cpu2017/benchspec/CPU
