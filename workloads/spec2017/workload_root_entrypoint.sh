1300f82e51717aed2dbb1f1766339e86a224732e
Alexander Symons <aesymons888@gmail.com>
2025-03-03 23:16:52 +0000

    Modified entrypoint to build app

--- workloads/spec2017/workload_root_entrypoint.sh
+++ workloads/spec2017/workload_root_entrypoint.sh
@@ -2,8 +2,15 @@
 #set -x
 APPNAME="$1"
 
+# Install spec
 mkdir -p $tmpdir/cpu2017_install
 sudo mount -t iso9660 -o ro,exec,loop $tmpdir/cpu2017-1_0_5.iso $tmpdir/cpu2017_install
 cd $tmpdir/cpu2017_install && echo "yes" | ./install.sh -d $tmpdir/cpu2017
 cp $tmpdir/memtrace.cfg $tmpdir/cpu2017/config/memtrace.cfg
 cp $tmpdir/compile-538-clang.sh $tmpdir/cpu2017/benchspec/CPU
+
+# Build the app
+cd ../cpu2017
+source shrc
+
+runcpu --config=memtrace --action=build $APPNAME
