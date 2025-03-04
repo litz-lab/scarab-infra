0874edc5ff815d99cc8902c3c917523ed83af3a2
Moonseop Kim <mkim293@ucsc.edu>
2025-03-04 01:37:34 +0000

    autoware dockerfile binary command

--- workloads/autoware/test.sh
+++
@@ -1,3 +0,0 @@
-/opt/ros/humble/bin/ros2 bag play $tmpdir/driving_log_replayer_output/yabloc/latest/sample/result_bag --rate 0.5 --clock 200 --topics /localization/kinematic_state -p & BAG_PID=$!
-ros2 service call /rosbag2_player/play_next rosbag2_interfaces/srv/PlayNext "{}"
-kill $BAG_PID
