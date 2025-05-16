8c3ae56c3e22dbee7c268e43bae5e7e7f04cf296
Alex <58124651+FlyingFish800@users.noreply.github.com>
2025-05-16 16:11:07 -0700

    Status option update (#143)
    * Started progress
    
    * Slurm print status enhanced
    
    * Updated to proposed formatting, retained per-node running job counts
    
    * Added warning when experiment not existant, and fixed edge case for stat runner
    
    * Added Failed - Slurm, and Non-existing columns
    
    * Added slurm failed to total_found and made Total row the calculated value
    
    * Patched a double counting issue

--- scarab_stats/stat_collector.py
+++ scarab_stats/stat_collector.py
@@ -7,8 +7,12 @@ import os
 os.chdir("scarab_stats")
 import sys
 sys.path.append(os.getcwd())
+sys.path.append(os.getcwd()+"/../scripts")
 
 import scarab_stats
+from utilities import read_descriptor_from_json
+
+print("START stat collector")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d','--descriptor_name', required=True, help='Experiment descriptor name. Usage: -d exp.json')
@@ -18,7 +22,38 @@ args = parser.parse_args()
 descriptor_name = args.descriptor_name
 outfile = args.outfile
 
-da = scarab_stats.stat_aggregator()
-print(f"Loading {descriptor_name}")
-E = da.load_experiment_json(descriptor_name, True)
-E.to_csv(f"{outfile}")
+json = read_descriptor_from_json(descriptor_name)
+if json is None:
+    print("Error: JSON file not found or invalid.")
+    exit(1)
+    
+root_directory = json["root_dir"] + "/simulations/" + json["experiment"] + "/logs/"
+
+log_files = os.listdir(root_directory)
+
+error_runs = []
+
+for file in log_files:
+    with open(root_directory+file, 'r') as f:
+        contents = f.read()
+        if 'Error' in contents:
+            error_runs += [file]
+
+if len(error_runs) > 0:
+    print(f"Not running due to {len(error_runs)} errors")
+    print("DONE")
+    exit(1)
+
+try:
+    da = scarab_stats.stat_aggregator()
+    print(f"Loading {descriptor_name}")
+    E = da.load_experiment_json(descriptor_name, True)
+    E.to_csv(f"{outfile}")
+
+    # This is the 'success' message
+    if E is not None:
+        print(f"Statistics collected for {json['experiment']}")
+except:
+    pass
+finally:
+    print("DONE")
