333ab4f51267658b94b3d9f1dcc4681074aa7009
Moonseop Kim <mkim293@ucsc.edu>
2025-05-17 00:10:58 +0000

    reorganizing autoware workload binary commands

--- workloads/autoware/lane_detector/src/my_lane_detector_node.cpp
+++ workloads/autoware/lane_detector/src/my_lane_detector_node.cpp
@@ -14,8 +14,11 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/core/ocl.hpp>
 
 #include <sensor_msgs/image_encodings.hpp>
+#include <sensor_msgs/msg/compressed_image.hpp>
+#include <opencv2/core/ocl.hpp>
 
 namespace my_ros2_module
 {
@@ -23,15 +26,19 @@ namespace my_ros2_module
 class LaneDetectorCpp : public rclcpp::Node
 {
 public:
+  using ImageMsg = sensor_msgs::msg::CompressedImage;
+
+  std::atomic_bool image_processed_;
   explicit LaneDetectorCpp(const rclcpp::NodeOptions & options)
   : Node("lane_detector_cpp", options),
     image_processed_(false)
   {
     using namespace std::placeholders;
 
-    sub_image_ = create_subscription<sensor_msgs::msg::Image>(
-      "/localization/pose_estimator/yabloc/image_processing/undistorted/image_raw", 10,
-      std::bind(&LaneDetectorCpp::imageCallback, this, _1));
+    sub_image_ = create_subscription<ImageMsg>(
+      "/sensing/camera/traffic_light/image_raw/compressed",
+      rclcpp::SensorDataQoS(),
+      std::bind(&LaneDetectorCpp::imageCallback, this, std::placeholders::_1));
 
     pub_image_ = create_publisher<sensor_msgs::msg::Image>(
       "/custom/line_detection_result_final", 10);
@@ -40,16 +47,14 @@ public:
   }
 
 private:
-  void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg)
+  void imageCallback(const ImageMsg::SharedPtr msg)
   {
-    cv_bridge::CvImagePtr cv_ptr;
-    try {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    } catch (std::exception & e) {
-      RCLCPP_ERROR(get_logger(), "cv_bridge exception: %s", e.what());
+    cv::Mat bgr_image =
+      cv::imdecode(cv::Mat(msg->data), cv::IMREAD_COLOR);
+    if (bgr_image.empty()) {
+      RCLCPP_ERROR(get_logger(), "imdecode failed");
       return;
     }
-    cv::Mat bgr_image = cv_ptr->image;
 
     cv::Mat gray_image;
     cv::cvtColor(bgr_image, gray_image, cv::COLOR_BGR2GRAY);
@@ -90,15 +95,12 @@ private:
     out_msg.image = final_img;
     pub_image_->publish(*out_msg.toImageMsg());
 
-    RCLCPP_INFO(get_logger(), "Published final processed image message.");
-
     image_processed_ = true;
-    rclcpp::shutdown();
+    rclcpp::shutdown(); 
   }
 
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_image_;
+  rclcpp::Subscription<ImageMsg>::SharedPtr sub_image_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_image_;
-  std::atomic_bool image_processed_;
 };
 
 }  // namespace my_ros2_module
@@ -106,31 +108,24 @@ private:
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
+  cv::ocl::setUseOpenCL(false);
+  cv::setNumThreads(1);
+
   auto node = std::make_shared<my_ros2_module::LaneDetectorCpp>(rclcpp::NodeOptions());
 
-  auto client_play_next = node->create_client<rosbag2_interfaces::srv::PlayNext>("/rosbag2_player/play_next");
-  
-  while (!client_play_next->wait_for_service(std::chrono::seconds(1))) {
-    RCLCPP_INFO(node->get_logger(), "Waiting for service /rosbag2_player/play_next...");
-    if (!rclcpp::ok()) {
-      RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for service.");
-      rclcpp::shutdown();
-      return 1;
-    }
-  }
+  auto play_node = rclcpp::Node::make_shared("combined_client_node");
 
-  auto request = std::make_shared<rosbag2_interfaces::srv::PlayNext::Request>();
-  RCLCPP_INFO(node->get_logger(), "Calling /rosbag2_player/play_next...");
-  auto future = client_play_next->async_send_request(request);
-  auto status = rclcpp::spin_until_future_complete(node, future);
-  if (status == rclcpp::FutureReturnCode::SUCCESS) {
-    RCLCPP_INFO(node->get_logger(), "Successfully called /rosbag2_player/play_next.");
-  } else {
-    RCLCPP_ERROR(node->get_logger(), "Failed to call /rosbag2_player/play_next.");
-    rclcpp::shutdown();
-    return 1;
-  }
+  rclcpp::executors::SingleThreadedExecutor exec;
+  exec.add_node(node);
+  exec.add_node(play_node);
 
-  rclcpp::spin(node);
+  auto client = play_node->create_client<rosbag2_interfaces::srv::PlayNext>(
+                  "/rosbag2_player/play_next");
+  client->wait_for_service();
+  client->async_send_request(std::make_shared<rosbag2_interfaces::srv::PlayNext::Request>());
+
+  exec.spin();
+  rclcpp::shutdown();
+
   return 0;
 }
