8c9d340bf7ffc7b7f8b603113996c4df0485df3b
Surim Oh <72247751+5surim@users.noreply.github.com>
2026-01-16 10:05:57 -0800

    Fix sim PARAMS per scarab hash (#273)
    - Export PARAMS.<arch> from each requested scarab git hash into the sim
      tree
    - Update memtrace/pt/exec run scripts to pick hash-specific PARAMS files
      when present

--- common/scripts/run_pt_single_simpoint.sh
+++ common/scripts/run_pt_single_simpoint.sh
@@ -14,6 +14,15 @@ SCARABHOME="$6"
 SCARAB_BIN="$7"
 SEGMENT_ID=0
 
+PARAMS_FILE="$SCARABHOME/src/PARAMS.$SCARABARCH"
+if [[ "$SCARAB_BIN" =~ ^scarab_([0-9a-fA-F]+) ]]; then
+  HASH="${BASH_REMATCH[1]}"
+  PARAMS_BY_HASH="$SCARABHOME/src/PARAMS.$SCARABARCH.$HASH"
+  if [ -f "$PARAMS_BY_HASH" ]; then
+    PARAMS_FILE="$PARAMS_BY_HASH"
+  fi
+fi
+
 if [ "$SEGMENT_ID" != "0" ]; then
   echo -e "PT trace simulation does not support simpoints currently. cluster id should always be 0."
   exit
@@ -29,7 +38,7 @@ OUTDIR=$SIMHOME
 segID=$SEGMENT_ID
 #echo "SEGMENT ID: $segID"
 mkdir -p $OUTDIR/$segID
-cp $SCARABHOME/src/PARAMS.$SCARABARCH $OUTDIR/$segID/PARAMS.in
+cp "$PARAMS_FILE" "$OUTDIR/$segID/PARAMS.in"
 cd $OUTDIR/$segID
 
 scarabCmd="$SCARABHOME/src/$SCARAB_BIN --full_warmup $WARMUP --frontend pt --cbp_trace_r0=$trace_home/$WORKLOAD_HOME/traces/pt/${traceMap} $SCARABPARAMS &> sim.log"
